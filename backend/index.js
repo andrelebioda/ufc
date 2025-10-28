2e4767e0d26fcee75713d2f2335a4ec72330232f
NooriBilal <bilal.noori77@icloud.com>
2025-10-28 19:00:13 +0100

    init commit

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.get("   /", (req, res) => {
+    res.send("Server läuft erfolgreich");
+});
+
+app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
