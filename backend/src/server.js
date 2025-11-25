4face48fca7ab4dc43fd45fc3db6d5e28684fcea
Andre Lebioda <andrelebioda@hotmail.de>
2025-11-25 17:25:00 +0100

    Port auf 5001 geändert

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import userRoutes from "./routes/fighterRoutes.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
